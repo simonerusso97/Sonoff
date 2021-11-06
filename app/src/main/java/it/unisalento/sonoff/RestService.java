fd26c9d00fed564bd16384f81a5f44220a193c7b
Simone Russo <34507615+simonerusso97@users.noreply.github.com>
2021-11-06 14:05:42 +0100

    Funzionante verso il gateway
    Effettua correttamente due chiamate, una get per la getStatus() all'avvio dell'app e una post  per cambiare lo stato del sonoff  con la changeStatus()

--- app/src/main/java/it/unisalento/sonoff/RestService.java
+++ app/src/main/java/it/unisalento/sonoff/RestService.java
@@ -8,14 +8,18 @@ import android.widget.Switch;
 import com.androidnetworking.AndroidNetworking;
 import com.androidnetworking.common.Priority;
 import com.androidnetworking.error.ANError;
+import com.androidnetworking.interfaces.JSONObjectRequestListener;
 import com.androidnetworking.interfaces.StringRequestListener;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class RestService {
     public RestService(Context context) {
         AndroidNetworking.initialize(context);
     }
 
-    String address = "http://172.20.10.4:3000";
+    String address = "http://192.168.1.67:3000";
 
     public void getStatus(Switch switcher){
         AndroidNetworking.get(address+"/getStatus")
@@ -30,27 +34,30 @@ public class RestService {
 
                     @Override
                     public void onError(ANError anError) {
-                        Log.w("Rest (getStatus()):", anError);
+                        Log.e("Rest (getStatus()):", anError.toString());
                     }
                 });
     }
 
     public void changeStatus(CompoundButton switcher, String status) {
-        AndroidNetworking.post(address+"changeStatus")
-                .addPathParameter("status", status)
+        AndroidNetworking.post(address+"/changeStatus")
+                .addBodyParameter("status", status)
                 .setPriority(Priority.LOW)
                 .build()
                 .getAsString(new StringRequestListener() {
+
                     @Override
                     public void onResponse(String response) {
-                        Log.w("Rest (changeStatus()):", "stato corrente " + status);
+                        Log.w("Rest (changeStatus()):", "stato corrente " + response);
                     }
 
                     @Override
                     public void onError(ANError anError) {
-                        Log.w("Rest (changeStatus()):", anError);
+                        Log.e("Rest (changeStatus()):", anError.toString());
                         switcher.setChecked(!status.equals("ON"));
                     }
                 });
+
+
     }
 }
